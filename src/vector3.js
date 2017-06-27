583bca853ccf7885b60ef6370ca5306b4b41eaf3
Owloid <jarett.lee.pi@gmail.com>
2017-06-27 09:22:45 -0400

    Add vector3 and start tests

---
+++ src/vector3.js
@@ -0,0 +1,201 @@
+// vector3.js
+'use strict';
+
+function create(x, y, z) {
+  return [x, y, z];
+}
+exports.create = create;
+
+function createBlank() {
+  return create(0, 0, 0);
+}
+
+function isValid(obj) {
+  return Array.isArray(obj) &&
+    obj.length === 3 &&
+    !isNaN(obj[0]) &&
+    !isNaN(obj[1]) &&
+    !isNaN(obj[2]);
+}
+exports.isValid = isValid;
+
+function getX(vector) {
+  return vector[0];
+}
+exports.getX = getX;
+
+function getY(vector) {
+  return vector[1];
+}
+exports.getY = getY;
+
+function getZ(vector) {
+  return vector[2];
+}
+exports.getZ = getZ;
+
+function setX(vector, val) {
+  vector[0] = val;
+}
+exports.setX = setX;
+
+function setY(vector, val) {
+  vector[1] = val;
+}
+exports.setY = setY;
+
+function setZ(vector, val) {
+  vector[2] = val;
+}
+exports.setZ = setZ;
+
+
+function equals(v1, v2) {
+  return getX(v1) === getX(v2) &&
+    getY(v1) === getY(v2) &&
+    getZ(v1) === getZ(v2);
+}
+exports.equals = equals;
+
+function sum(v1, v2) {
+  let sum = createBlank();
+
+  setX(sum, getX(v1) + getX(v2));
+  setY(sum, getY(v1) + getY(v2));
+  setZ(sum, getZ(v1) + getZ(v2));
+
+  return sum;
+}
+exports.sum = sum;
+
+// v1 minuend
+// v2 subtrahend
+function difference(v1, v2) {
+  let difference = createBlank();
+
+  setX(difference, getX(v1) - getX(v2));
+  setY(difference, getY(v1) - getY(v2));
+  setZ(difference, getZ(v1) - getZ(v2));
+
+  return difference;
+}
+exports.difference = difference;
+
+// aka Hadamard product
+function entrywiseProduct(v1, v2) {
+  let product = createBlank();
+
+  setX(product, getX(v1) * getX(v2));
+  setY(product, getY(v1) * getY(v2));
+  setZ(product, getZ(v1) * getZ(v2));
+
+  return product;
+}
+exports.entrywiseProduct = entrywiseProduct;
+
+function scalarMult(vector, scalar) {
+  let product = createBlank();
+
+  setX(product, getX(vector) * scalar);
+  setY(product, getY(vector) * scalar);
+  setZ(product, getZ(vector) * scalar);
+
+  return product;
+}
+exports.scalarMult = scalarMult;
+
+function dotProduct(v1, v2) {
+  let x = getX(v1) * getX(v2);
+  let y = getY(v1) * getY(v2);
+  let z = getZ(v1) * getZ(v2);
+
+  return x + y + z;
+}
+exports.dotProduct = dotProduct;
+
+function crossProduct(v1, v2) {
+  let product = createBlank();
+
+  let v1_x = getX(v1);
+  let v1_y = getY(v1);
+  let v1_z = getZ(v1);
+  let v2_x = getX(v2);
+  let v2_y = getY(v2);
+  let v2_z = getZ(v2);
+
+  setX(product, v1_y * v2_z - v2_y * v1_z);
+  setY(product, v1_z * v2_x - v2_z * v1_x);
+  setZ(product, v1_x * v2_y - v2_y * v1_x);
+
+  return product;
+}
+exports.crossProduct = crossProduct;
+
+function sqMagnitude(vector) {
+  let x = getX(vector);
+  let y = getY(vector);
+  let z = getZ(vector);
+
+  return x * x + y * y + z * z;
+}
+exports.sqMagnitude = sqMagnitude;
+
+function magnitude(vector) {
+  return Math.sqrt(sqMagnitude(vector));
+}
+exports.magnitude = magnitude;
+
+function normalize(vector) {
+  let mag = magnitude(vector);
+  return scalarMult(vector, 1/mag);
+}
+exports.normalize = normalize;
+
+function sqDistance(v1, v2) {
+  let dv = difference(v1, v2);
+  return sqMagnitude(dv);
+}
+exports.sqDistance = sqDistance;
+
+function distance(v1, v2) {
+  let dv = difference(v1, v2);
+  return magnitude(dv);
+}
+exports.distance = distance;
+
+// projection of v1 onto v2
+function projection(v1, v2) {
+  let u = dotProduct(v1, v2);
+  let sqMag = sqMagnitude(v2);
+  return scalarMult(u, 1/sqMag);
+}
+exports.projection = projection;
+
+// TODO
+/*
+function complement(v1, v2) {
+  let u = dotProduct(v1, v2);
+}
+*/
+
+// TODO
+/*
+function squareDistanceToLine(vector, line) {
+  let w1 = subtract(vector, line[0]);
+  let w2 = subtract(line[1], line[0]);
+  project(w2, w1, w2);
+  return squareDistance(w1, w2);
+}
+prototype.squareDistanceToLine = squareDistanceToLine;
+function squareDistanceToLineSegment(vector, line) {
+  let w1 = subtract([0, 0, 0], vector, line[0]);
+  let w2 = subtract([0, 0, 0], line[1], line[0]);
+  let wx = w2[0];
+  project(w2, w1, w2);
+  let dwx = w2[0] / wx;
+  if (dwx * dwx < 1) return squareDistance(w1, w2);
+  if (dwx < 0) return squareDistance(vector, line[0]);
+  return squareDistance(vector, line[1]);
+}
+prototype.squareDistanceToLineSegment = squareDistanceToLineSegment;
+*/
---
+++ test/test.js
@@ -0,0 +1,113 @@
+const assert = require('assert');
+const vector3 = require('../src/vector3.js')
+
+describe('vector3', function() {
+  var vectors;
+  beforeEach(function(){
+    vectors = [];
+    vectors[0] = vector3.create(1, 2, 3);
+    vectors[1] = vector3.create(4, 8, 3);
+    vectors[2] = vector3.create(5, 0, 3);
+    vectors[3] = vector3.create(4, 8, -2);
+    vectors[4] = vector3.create(-19, -21, 5);
+    vectors[5] = vector3.create(-31, 10, -8);
+    vectors[6] = vector3.create(-12, -3, -4);
+    vectors[7] = vector3.create(8, 10, 3);
+  });
+  afterEach(function(){
+    expected = [];
+    expected[0] = vector3.create(1, 2, 3);
+    expected[1] = vector3.create(4, 8, 3);
+    expected[2] = vector3.create(5, 0, 3);
+    expected[3] = vector3.create(4, 8, -2);
+    expected[4] = vector3.create(-19, -21, 5);
+    expected[5] = vector3.create(-31, 10, -8);
+    expected[6] = vector3.create(-12, -3, -4);
+    expected[7] = vector3.create(8, 10, 3);
+    for (var i = 0; i < vectors.length; i++) {
+      assert(vector3.equals(vectors[i], expected[i]));
+    }
+  });
+
+  describe('#equals()', function() {
+    it('should be equal', function() {
+      let v1 = vector3.create(1, 2, 3);
+      let v2 = vector3.create(1, 2, 3);
+      assert(vector3.equals(v1, v2));
+    });
+    it('should be equal', function() {
+      let v1 = vector3.create(5, 10, 3);
+      let v2 = vector3.create(5, 10, 3);
+      assert(vector3.equals(v1, v2));
+    });
+    it('should not be equal', function() {
+      let v1 = vector3.create(5, 0, 11);
+      let v2 = vector3.create(5, 1, 11);
+      assert(!vector3.equals(v1, v2));
+    });
+    it('should not be equal', function() {
+      let v1 = vector3.create(-5, 0, -11);
+      let v2 = vector3.create(-5, 0, -12);
+      assert(!vector3.equals(v1, v2));
+    });
+    it('should not be equal', function() {
+      let v1 = vector3.create(5, 10, 3);
+      let v2 = vector3.create(4, 10, 3);
+      assert(!vector3.equals(v1, v2));
+    });
+  });
+
+  describe('#sum()', function() {
+    it('should give the vectors sum', function() {
+      let result = vector3.sum(vectors[0], vectors[1]);
+      let expected = vector3.create(5, 10, 6);
+      assert(vector3.isValid(result));
+      assert(vector3.equals(result, expected));
+    });
+    it('should give the vectors sum', function() {
+      let result = vector3.sum(vectors[2], vectors[3]);
+      let expected = vector3.create(9, 8, 1);
+      assert(vector3.isValid(result));
+      assert(vector3.equals(result, expected));
+    });
+    it('should give the vectors sum', function() {
+      let result = vector3.sum(vectors[4], vectors[5]);
+      let expected = vector3.create(-50, -11, -3);
+      assert(vector3.isValid(result));
+      assert(vector3.equals(result, expected));
+    });
+    it('should give the vectors sum', function() {
+      let result = vector3.sum(vectors[6], vectors[7]);
+      let expected = vector3.create(-4, 7, -1);
+      assert(vector3.isValid(result));
+      assert(vector3.equals(result, expected));
+    });
+  });
+
+  describe('#difference()', function() {
+    it('should give the vector difference', function() {
+      let result = vector3.difference(vectors[0], vectors[1]);
+      let expected = vector3.create(-3, -6, 0);
+      assert(vector3.isValid(result));
+      assert(vector3.equals(result, expected));
+    });
+    it('should give the vector difference', function() {
+      let result = vector3.difference(vectors[2], vectors[3]);
+      let expected = vector3.create(1, -8, 5);
+      assert(vector3.isValid(result));
+      assert(vector3.equals(result, expected));
+    });
+    it('should give the vector difference', function() {
+      let result = vector3.difference(vectors[4], vectors[5]);
+      let expected = vector3.create(12, -31, 13);
+      assert(vector3.isValid(result));
+      assert(vector3.equals(result, expected));
+    });
+    it('should give the vector difference', function() {
+      let result = vector3.difference(vectors[6], vectors[7]);
+      let expected = vector3.create(-20, -13, -7);
+      assert(vector3.isValid(result));
+      assert(vector3.equals(result, expected));
+    });
+  });
+});
